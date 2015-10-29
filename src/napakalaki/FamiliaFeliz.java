eb1b645a806d6e4b77cdddf57aa7986aab46b632
juanmfajardo <jmfajardo@Juans-MacBook-Air.local>
2015-10-29 19:48:47 +0100

    Familia Feliz (java)

--- src/napakalaki/FamiliaFeliz.java
+++ src/napakalaki/FamiliaFeliz.java
@@ -4,12 +4,54 @@
  * and open the template in the editor.
  */
 package napakalaki;
+
+import java.util.ArrayList;
+
 /**
  *
  * @author Ismael
  */
 public class FamiliaFeliz {
-    private static int num_miembros;
+    private static int num_miembros = 5;
     private String nombre;
-    private Monster componentes;
+    private ArrayList<Monster> componentes;
+    
+    public FamiliaFeliz (String nomb, ArrayList<Monster> comp){
+        nombre = nomb;
+        componentes = comp;
+    }
+
+    public boolean incluirMiembro(Monster monstruo){
+        if (componentes.size()<5){
+            componentes.add(monstruo);
+            return true;
+        }
+        else
+            return false;
+    }
+    
+    public float nivelMedio(){
+        float media = 0;
+        int contador = 0;
+        for(Monster mons : componentes){
+            media = media + mons.getCombatLevel();
+            contador++;
+        }
+        
+        media = media / contador;
+        return media;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        
+        FamiliaFeliz una_familia = new FamiliaFeliz("5 primeros", new ArrayList());
+        System.out.println(monst.toString());
+        
+        
+    }
+
 }
+
+
+//        for(Monster monst : monstruos){
