754b08b97678cee208aca3c732d2dab676fde39b
sanchezismael <sanchezismael@correo.ugr.es>
2015-10-30 13:20:05 +0100

    P2: Empezada clase Napakalaki

---
+++ src/napakalaki/Napakalaki.java
@@ -0,0 +1,54 @@
+package napakalaki;
+
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ismael y Juan
+ */
+public class Napakalaki {
+    private static final Napakalaki instance = new Napakalaki();
+    private Napakalaki(){
+        
+    }
+    
+    private void initPlayers(ArrayList<String> names){
+        
+    }
+    
+//    Player nextPlayer(){
+//        
+//    }
+    
+    private boolean nextTurnAllowed(){
+        return true;
+    }
+    
+    private void setEnemies(){
+        
+    }
+    
+    public static Napakalaki getInstance(){
+        return instance;
+    }
+    
+    public CombatResult developCombat(){
+        
+    }
+    
+    public void discardVisibleTreasures(ArrayList<TreasureKind> treasures){
+        
+    }
+    
+    public void discardHiddenTreasures(ArrayList<TreasureKind> treasures){
+        
+    }
+    
+}
