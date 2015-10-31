2048f8bec8dbd7e224e4169e252195c6959325b3
sanchezismael <sanchezismael@correo.ugr.es>
2015-10-31 12:20:17 +0100

    P2: Terminada la declaracion de la clase Treasure

---
+++ src/napakalaki/Treasure.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+/**
+ *
+ * @author Ismael
+ */
+public class Treasure {
+    private String name;
+    private int bonus;
+    private TreasureKind type;
+    
+    public Treasure(String n, int bonus, TreasureKind t){
+        
+    }
+    
+    public String getName(){
+        
+    }
+    
+    public int getBonus(){
+        
+    }
+    
+    public TreasureKind getType(){
+        
+    }
+}
