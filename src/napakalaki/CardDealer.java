3b3e5e84dc3101590c8ec55f220359f0ee1d9d45
juanmfajardo <juanmfajardo@192.168.1.129>
2016-01-08 12:16:44 +0100

    Revert "Revert "algo""
    This reverts commit 20043674cb13969fc098c9fe58f51b02b69cc31a.

--- src/napakalaki/CardDealer.java
+++ src/napakalaki/CardDealer.java
@@ -29,7 +29,7 @@ public class CardDealer {
         usedTreasures = new ArrayList();
         unusedMonsters = new ArrayList();
         usedMonsters = new ArrayList();
-       
+        unusedCultists = new ArrayList();
     }
     
     private void initTreasureCardDeck(){
@@ -313,5 +313,6 @@ public class CardDealer {
     public void initCards(){
         initTreasureCardDeck();
         initMonsterCardDeck();
+        initCultistCardDeck();
     }
 }
