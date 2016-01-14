fa1b97cd40ab260ff287357871c49b0acea63baa
juanmfajardo <juanmfajardo@192.168.1.129>
2016-01-14 23:53:20 +0100

    P5: Diseño e implementación de vistas

--- src/napakalaki/Main.java
+++ src/napakalaki/Main.java
@@ -7,19 +7,28 @@ package napakalaki;
 
 import GUI.Dice;
 import GUI.NapakalakiView;
+import GUI.PlayerNamesCapture;
+import java.util.ArrayList;
 
 /**
  *
  * @author juanmfajardo
  */
 public class Main {
-    
+            
     public static void main(String[] args) {
       Napakalaki game = Napakalaki.getInstance();
       NapakalakiView napakalakiView = new NapakalakiView();
       Dice.createInstance (napakalakiView);
       napakalakiView.setNapakalaki(game);
+      
+      ArrayList<String> names = new ArrayList();
+      PlayerNamesCapture namesCapture = new PlayerNamesCapture(napakalakiView,true);
+      
+      names = namesCapture.getNames();
+      game.initGame(names);
+      
       napakalakiView.setVisible(true);
-              
+
     }
 }
